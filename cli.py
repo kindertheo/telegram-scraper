37556d63af761a1edeb6e010b776eb8a3abdef27
théo <kinder.theo@gmail.com>
2022-06-02 21:48:01 +0200

    create command scrape

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import click
+from click_default_group import DefaultGroup
 from config import config_manager
 
 import inquirer
@@ -17,7 +18,6 @@ def interactive_select_config(choices):
 
 @click.group()
 def main():
-    choices= [1,2,3]
     pass
 
 @main.command()
@@ -37,8 +37,20 @@ def select_config():
     choices = config_manager().select_section()
     response = interactive_select_config(choices) 
     conf = config_manager().select_section_from_number(response['config'])
-    
+    print(conf)
+    return conf
 
+@main.command()
+@click.pass_context
+def scrape(ctx):
+    print("hello world")
+    conf = ctx.forward(select_config)
+    ids = config_manager().get_datas_from_config(conf)
+    telegram.run(ids)
+
+@main.command()
+def hello():
+    print("hello")
 
 # @main.command()
 # @click.option("config")
