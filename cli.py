7f01e9758e768bbb984188802c1a23bb22926278
Théo <kinder.theo@gmail.com>
2022-06-06 02:22:11 +0200

    add scrape-one and invit command

--- cli.py
+++ cli.py
@@ -4,13 +4,14 @@ from config import config_manager
 
 import inquirer
 from inquirer.themes import GreenPassion
+from csv_manager import get_csv_files, read_csv
 
 from telegram import telegram
 
-def interactive_select_config(choices):
+def interactive_select_config(choices, message: str = "Choose one"):
     q = [
         inquirer.List('config',
-                    message='Choose a number',
+                    message=message,
                     choices=choices,
                     default='no'),
     ]
@@ -44,21 +45,42 @@ def select_config():
 
 @main.command()
 @click.pass_context
-def scrape(ctx):
-    print("hello world")
+def scrape_all(ctx):
     conf = ctx.forward(select_config)
     ids = config_manager().get_datas_from_config(conf)
     telegram(api_id=ids['api_id'],
             api_hash=ids['api_hash'],
             phone=ids['phone']
-    ).run()
+    ).get_users()
 
 @main.command()
-def hello():
-    print("hello")
+@click.pass_context
+def scrape_one(ctx):
+    conf = ctx.forward(select_config)
+    ids = config_manager().get_datas_from_config(conf)
+    tlg = telegram(api_id=ids['api_id'],
+            api_hash=ids['api_hash'],
+            phone=ids['phone']) #.get_users_by_channel()
+    channels = tlg.get_channels()
+    print(channels)
+    response = interactive_select_config(channels)
+    tlg.get_users_from_channel(response['config'])
 
-# @main.command()
-# @click.option("config")
+@main.command()
+@click.pass_context
+def invit(ctx):
+    conf = ctx.forward(select_config)
+    ids = config_manager().get_datas_from_config(conf)
+    tgm = telegram(api_id=ids['api_id'],
+            api_hash=ids['api_hash'],
+            phone=ids['phone'])
+    channels = tgm.get_channels()
+    files = get_csv_files()
+    response = interactive_select_config(files, message="Choose a CSV file to import in your Channel/Group")
+    users = read_csv(response['config'])
+    channel_response = interactive_select_config(channels, message="Choose a channel to invit")
+    # print(users)
+    tgm.invit_users(channel_response['config'], users)
 
 if __name__ == "__main__":
     main()
